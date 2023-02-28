cd840842d813ea46839715a1cca745efc71d5735
Nirav <niravshahcolab@gmail.com>
2023-02-28 15:10:45 +0000

    Setup update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='mdfenicsx',
     version="0.1",
-    description="Mesh deformation using FEniCSx"
+    description="Mesh deformation using FEniCSx",
     url='https://github.com/niravshah241/MDFEniCSx',
     author='Nirav Shah',
     author_email='niravshahcolab@gmail.com',
