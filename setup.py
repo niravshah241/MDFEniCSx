804c72c860470bc58988935c61b261218705c617
Nirav <niravshahcolab@gmail.com>
2023-02-28 13:00:40 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mdfenicsx',
+    version="dev",
+
+    url='https://github.com/niravshah241/MDFEniCSx',
+    author='Nirav Shah',
+    author_email='niravshahcolab@gmail.com',
+
+    py_modules=['mdfenicsx'],
+)
