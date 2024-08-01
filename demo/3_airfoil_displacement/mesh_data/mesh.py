6b08ca75293c8b87f58da9a830af423aec00d598
niravshah241 <niravshahcolab@gmail.com>
2024-08-01 19:57:03 +0100

    Demos added in CI

--- demo/3_airfoil_displacement/mesh_data/mesh.py
+++ demo/3_airfoil_displacement/mesh_data/mesh.py
@@ -1,4 +1,4 @@
-import sys,os
+import os
 
 import gmsh
 import numpy as np
@@ -12,7 +12,7 @@ dir = os.path.dirname(__file__)
 gmsh.initialize()
 
 lc = 0.2
-num_airfoil_refinement = 100   
+num_airfoil_refinement = 100
 
 L = 4
 H = 1
@@ -20,44 +20,62 @@ gdim = 2
 
 """
     Defining the shape of the airfoil using Bézier Curves
-    Geometrical parameters of the airfoil according to Xiaoqiang et al 
+    Geometrical parameters of the airfoil according to Xiaoqiang et al
 """
-c_1, c_2 = 0.5, 0.5                                     # Coefficients of camber-line-abscissa parameter equation
-c_3, c_4 = 0.1, 0.05                                    # Coefficients of camber-line.ordinate parameter equation
-coeff_thick = [0.2969,-0.126,-0.3516,0.2843,-0.1036]    # Coefficients of the thickness equation taken from the NACA 2412 Airfoil
+# Coefficients of camber-line-abscissa parameter equation
+c_1, c_2 = 0.5, 0.5
+# Coefficients of camber-line.ordinate parameter equation
+c_3, c_4 = 0.1, 0.05
+# Coefficients of the thickness equation taken from the NACA 2412 Airfoil
+coeff_thick = [0.2969, -0.126, -0.3516, 0.2843, -0.1036]
 
 # Bézier curves with control parameter k \in [0,1]
-x_c = lambda k: 3 * c_1 * k * (1 - k)**2 + 3 * c_2 * (1-k) * k**2 + k**3    # Camber-line-abscissa
-y_c = lambda k: 3 * c_3 * k * (1 - k)**2 + 3 * c_4 * (1-k) * k**2           # Camber-line-abscissa
+
+
+def x_c(k): return 3 * c_1 * k * (1 - k)**2 + 3 * c_2 * \
+    (1-k) * k**2 + k**3    # Camber-line-abscissa
+
+
+def y_c(k): return 3 * c_3 * k * (1 - k)**2 + 3 * c_4 * \
+    (1-k) * k**2           # Camber-line-abscissa
 
 # Thickness equation for a 6% thick airfoil
-thickness = lambda x: 0.06 / 0.1 * (coeff_thick[0] * np.sqrt(x) + coeff_thick[1] * x + coeff_thick[2] * x**2 + coeff_thick[3] * x**3 + coeff_thick[4] * x**4)
 
-# Position of the airfoil in the computational domain defined by the coordinates of the leading edge
-leading_edge_x = np.min([L/8,0.5])
+
+def thickness(x):
+    return 0.06 / 0.1 * (coeff_thick[0] * np.sqrt(x) + coeff_thick[1] * x +
+                         coeff_thick[2] * x**2 + coeff_thick[3] * x**3 +
+                         coeff_thick[4] * x**4)
+
+
+# Position of the airfoil in the computational domain
+# defined by the coordinates of the leading edge
+leading_edge_x = np.min([L/8, 0.5])
 leading_edge_y = H/2
 
 # Upper and lower surface of the airfoil
 x_u = x_l = lambda k: leading_edge_x + x_c(k)
-y_u = lambda k: leading_edge_y + y_c(k) + thickness(x_c(k))
-y_l = lambda k: leading_edge_y + y_c(k) - thickness(x_c(k))
+def y_u(k): return leading_edge_y + y_c(k) + thickness(x_c(k))
+def y_l(k): return leading_edge_y + y_c(k) - thickness(x_c(k))
+
 
 # Calculate maximal thickness of the airfoil
-thickness_max = np.max([thickness(x) for x in np.linspace(0,1,num_airfoil_refinement)])
+thickness_max = np.max([thickness(x)
+                       for x in np.linspace(0, 1, num_airfoil_refinement)])
 
 """
     Meshing the airfoil using gmsh
 """
 
-rectangle = gmsh.model.occ.addRectangle(0,0,0, L, H, tag=3)
+rectangle = gmsh.model.occ.addRectangle(0, 0, 0, L, H, tag=3)
 # Define lower curve of the airfoil using the BSplines and given points
-points_lower_curve=[]
-for k in np.linspace(0,1,num_airfoil_refinement):
+points_lower_curve = []
+for k in np.linspace(0, 1, num_airfoil_refinement):
     points_lower_curve.append(gmsh.model.occ.addPoint(x_l(k), y_l(k), 0.0, lc))
 
 # Define upper curve of the airfoil using the BSplines and given points
-points_upper_curve=[points_lower_curve[0]]
-for k in np.linspace(0,1,num_airfoil_refinement)[1:-1]:
+points_upper_curve = [points_lower_curve[0]]
+for k in np.linspace(0, 1, num_airfoil_refinement)[1:-1]:
     points_upper_curve.append(gmsh.model.occ.addPoint(x_u(k), y_u(k), 0.0, lc))
 points_upper_curve.append(points_lower_curve[-1])
 
@@ -65,8 +83,8 @@ C1 = gmsh.model.occ.addBSpline(points_lower_curve, degree=3)
 C2 = gmsh.model.occ.addBSpline(points_upper_curve, degree=3)
 
 # Create airfoil and cut out of computational domain
-W = gmsh.model.occ.addWire([C1,C2])
-obstacle=gmsh.model.occ.addPlaneSurface([W])
+W = gmsh.model.occ.addWire([C1, C2])
+obstacle = gmsh.model.occ.addPlaneSurface([W])
 
 # Remove points of the airfoil
 for i in list(dict.fromkeys(points_lower_curve + points_upper_curve)):
@@ -79,12 +97,17 @@ gmsh.model.occ.synchronize()
 # Create a distance field to the airfoil
 distance_field = gmsh.model.mesh.field.add("Distance")
 gmsh.model.mesh.field.setNumbers(distance_field, "CurvesList", [C1, C2])
-gmsh.model.mesh.field.setNumbers(distance_field,"PointsList", [points_lower_curve[0],points_lower_curve[-1]])
-gmsh.model.mesh.field.setNumber(distance_field,"Sampling", num_airfoil_refinement*2)
+gmsh.model.mesh.field.setNumbers(distance_field, "PointsList", [
+                                 points_lower_curve[0],
+                                 points_lower_curve[-1]])
+gmsh.model.mesh.field.setNumber(
+    distance_field, "Sampling", num_airfoil_refinement*2)
 # Create refined mesh using a threshold field
-refinement= gmsh.model.mesh.field.add("Threshold")
+refinement = gmsh.model.mesh.field.add("Threshold")
 gmsh.model.mesh.field.setNumber(refinement, "IField", distance_field)
-# Set the refinement levels (LcMin for the mesh size in the refined region, LcMax for the mesh size far from the refined region)
+# Set the refinement levels
+# (LcMin for the mesh size in the refined region,
+# LcMax for the mesh size far from the refined region)
 gmsh.model.mesh.field.setNumber(refinement, "LcMin", lc/5)
 gmsh.model.mesh.field.setNumber(refinement, "LcMax", lc*2)
 # Set the threshold value where which refinement should be applied
@@ -108,7 +131,6 @@ gmsh.model.mesh.generate(gdim)
 gmsh.model.mesh.setOrder(1)
 # Mesh optimisation
 gmsh.model.mesh.optimize("Netgen")
-
 
 
 """
@@ -148,7 +170,7 @@ gmsh.model.occ.remove(gmsh.model.getEntities(dim=gdim-1))
 gmsh.model.occ.remove(gmsh.model.getEntities(dim=gdim))
 gmsh.model.occ.remove(gmsh.model.getEntities(dim=0))
 
-gmsh.write(os.path.join(dir,"mesh.msh"))
+gmsh.write(os.path.join(dir, "mesh.msh"))
 
 try:
     gmsh.fltk.run()
@@ -159,9 +181,10 @@ except Exception:
 gmsh_model_rank = 0
 mesh_comm = MPI.COMM_WORLD
 
-mesh, cell_tags, facet_tags = dolfinx.io.gmshio.read_from_msh(os.path.join(dir,"mesh.msh"), mesh_comm, gmsh_model_rank, gdim=gdim)
+mesh, cell_tags, facet_tags = dolfinx.io.gmshio.read_from_msh(
+    os.path.join(dir, "mesh.msh"), mesh_comm, gmsh_model_rank, gdim=gdim)
 
-with dolfinx.io.XDMFFile(MPI.COMM_WORLD, "mesh.xdmf","w") as mesh_file_xdmf:
+with dolfinx.io.XDMFFile(MPI.COMM_WORLD, "mesh.xdmf", "w") as mesh_file_xdmf:
     mesh_file_xdmf.write_mesh(mesh)
     mesh_file_xdmf.write_meshtags(cell_tags, mesh.geometry)
     mesh_file_xdmf.write_meshtags(facet_tags, mesh.geometry)
